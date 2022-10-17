b4ea4caa9d4ebf5cc47cdde17de178e63ad83fd9
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 11:10:01 -0400

    rtk query ApiProvider and api implemented in index.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 /* ---- RTK Query ---- */
+import { ApiProvider } from '@reduxjs/toolkit/query/react'
+import { pokeAPI } from './api/pokeapi/pokeAPI.slice'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={pokeAPI}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 )
