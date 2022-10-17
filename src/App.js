f137b5ff091926670f2bb026e5cc78f18cb1daf3
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 10:21:16 -0400

    init app.js and app.styles.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <></>
+/* ---- Components ---- */
+import Layout from './components/layout/layout.component'
+import RandomPokemon from './components/random-pokemon/random-pokemon.component'
+
+/* ---- Styles ---- */
+import { GlobalStyle } from './App.styles'
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+
+      <Layout>
+        <RandomPokemon />
+      </Layout>
+    </>
+  )
 }
 
 export default App
