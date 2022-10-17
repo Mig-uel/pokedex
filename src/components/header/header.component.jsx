1c4206492bc8aaa506c30e94b3967d875aea04e4
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 18:17:27 -0400

    using cached color property.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,13 @@
+import { useGetRandomPokemonQuery } from '../../api/pokeapi/pokeAPI.slice'
+
 /* ---- Styles ---- */
 import { Container, ImageContainer, Image } from './header.styles'
 
 const Header = () => {
+  const { data: randomPokemonData } = useGetRandomPokemonQuery()
+
   return (
-    <Container>
+    <Container background={randomPokemonData?.color.name}>
       <ImageContainer>
         <Image
           src='https://external-preview.redd.it/tQged7mKJ3cUpNMq5IMeceZvyKP3cTyHqhNmKEQ0Vv8.png?width=640&crop=smart&auto=webp&s=5fc89334e792e2c9b294d1d328bf522cdede4cdf'
