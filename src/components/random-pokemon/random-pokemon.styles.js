72cab093068b7cb266caabe9626f709c6000cf5f
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 14:23:59 -0400

    name and background colors dynamic.

--- src/components/random-pokemon/random-pokemon.styles.js
+++ src/components/random-pokemon/random-pokemon.styles.js
@@ -1,23 +1,52 @@
 import styled from 'styled-components'
 
-export const RandomPokemonContainer = styled.div`
+export const Container = styled.div`
   height: 100vh;
   width: 100%;
 
-  background-color: ${(props) => props.background};
-  position: absolute;
-  z-index: -1;
-
-  top: 0;
-
-  display: flex;
-  flex-direction: column;
-
-  align-content: center;
-`
-export const LoadingContainer = styled.div`
+  background: ${(props) => props.background};
   position: relative;
 
-  top: 50%;
-  left: 50%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  gap: 10px;
+`
+
+export const RandomPokemonContainer = styled.div`
+  text-align: center;
+`
+
+export const SpriteContainer = styled.img`
+  width: 150px;
+`
+
+export const NameContainer = styled.h1`
+  font-size: 2.5rem;
+  color: ${(props) => {
+    if (props.background === 'white') return '#000'
+    else if (props.background === 'black') return '#fff'
+    else if (props.background === 'yellow') return '#000'
+    else return '#fff'
+  }};
+`
+
+export const ViewMoreButton = styled.div`
+  position: absolute;
+  bottom: 30px;
+
+  color: ${(props) => props.background};
+
+  text-align: center;
+
+  background: ${(props) => props.buttonColor};
+  border-radius: 50px;
+
+  padding: 20px;
+
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `
