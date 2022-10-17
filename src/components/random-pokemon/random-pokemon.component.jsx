33debf1200918244532b46a774dbf34e2cdd39ab
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 14:23:35 -0400

    new colors obj, conditionally rendering random pokemon container based on query loading.

--- src/components/random-pokemon/random-pokemon.component.jsx
+++ src/components/random-pokemon/random-pokemon.component.jsx
@@ -1,15 +1,53 @@
 /* ---- Styles ---- */
-import { RandomPokemonContainer } from './random-pokemon.styles'
+import {
+  Container,
+  RandomPokemonContainer,
+  SpriteContainer,
+  NameContainer,
+  ViewMoreButton,
+} from './random-pokemon.styles'
 
+import { BsArrowDownCircleFill } from 'react-icons/bs'
+
+/* ---- RTK Query ---- */
 import { useGetRandomPokemonQuery } from '../../api/pokeapi/pokeAPI.slice'
 
 const RandomPokemon = () => {
-  const { data: randomPokemon, isLoading: isRandomLoading } =
+  const { data: randomPokemonData, isLoading: isRandomLoading } =
     useGetRandomPokemonQuery()
-  console.log('Random Pokemon', randomPokemon)
-  console.log('isRandomLoading', isRandomLoading)
 
-  return <RandomPokemonContainer></RandomPokemonContainer>
+  const colors = {}
+
+  console.log('Random Pokemon', randomPokemonData)
+
+  if (
+    randomPokemonData?.color.name === 'black' ||
+    randomPokemonData?.color.name === 'blue'
+  ) {
+    colors.buttonColor = '#fff'
+  } else colors.buttonColor = '#000'
+
+  const randomPokemonSnippet = !isRandomLoading && (
+    <Container background={randomPokemonData?.color.name}>
+      <RandomPokemonContainer>
+        <SpriteContainer
+          src={randomPokemonData.sprites.front_default}
+          alt={randomPokemonData.name}
+        />
+        <NameContainer background={randomPokemonData.color.name}>
+          {randomPokemonData.name}
+        </NameContainer>
+      </RandomPokemonContainer>
+      <ViewMoreButton
+        background={randomPokemonData?.color.name}
+        buttonColor={colors.buttonColor}
+      >
+        <BsArrowDownCircleFill /> View All Pokemon
+      </ViewMoreButton>
+    </Container>
+  )
+
+  return <>{isRandomLoading ? <></> : randomPokemonSnippet}</>
 }
 
 export default RandomPokemon
