4c5f61404d2cc310cb85e4778bb74044f23dca8c
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 11:10:39 -0400

    implemented random pokemon hook.

--- src/components/random-pokemon/random-pokemon.component.jsx
+++ src/components/random-pokemon/random-pokemon.component.jsx
@@ -1,10 +1,14 @@
 /* ---- Styles ---- */
-import {
-  RandomPokemonContainer,
-  LoadingContainer,
-} from './random-pokemon.styles'
+import { RandomPokemonContainer } from './random-pokemon.styles'
+
+import { useGetRandomPokemonQuery } from '../../api/pokeapi/pokeAPI.slice'
 
 const RandomPokemon = () => {
+  const { data: randomPokemon, isLoading: isRandomLoading } =
+    useGetRandomPokemonQuery()
+  console.log('Random Pokemon', randomPokemon)
+  console.log('isRandomLoading', isRandomLoading)
+
   return <RandomPokemonContainer></RandomPokemonContainer>
 }
 
