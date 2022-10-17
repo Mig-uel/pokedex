d4db20535f4e4565d3c190e4ee5e244782240167
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-17 18:18:26 -0400

    header is fixed, background color depends on the pokemon, added border radius.

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -8,8 +8,22 @@ export const Container = styled.div`
   justify-content: center;
 
   padding: 15px 0;
-  position: absolute;
+  position: fixed;
   z-index: 1;
+
+  background: ${(props) => {
+    if (props.background === 'white') return '#ECF0F1'
+    else if (props.background === 'gray') return '#95A5A6'
+    else if (props.background === 'blue') return '#3498DB'
+    else if (props.background === 'green') return '#27AE60'
+    else if (props.background === 'red') return '#C0392B'
+    else if (props.background === 'purple') return '#8E44AD'
+    else if (props.background === 'yellow') return '#F1C40F'
+    else return props.background
+  }};
+
+  border-bottom-left-radius: 30px;
+  border-bottom-right-radius: 30px;
 `
 
 export const ImageContainer = styled.div`
