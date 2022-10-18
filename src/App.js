3ef489600b659f86209fbc400d8ecc4c057bd506
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-18 18:32:05 -0400

    added pokeball loader.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
+import {
+  useGetPokemonQuery,
+  useGetRandomPokemonQuery,
+} from './api/pokeapi/pokeAPI.slice'
+
 /* ---- Components ---- */
 import Layout from './components/layout/layout.component'
 import RandomPokemon from './components/random-pokemon/random-pokemon.component'
 
 /* ---- Styles ---- */
 import { GlobalStyle } from './App.styles'
+import Loader from './components/loader/loader.component'
 
 const App = () => {
+  const { isLoading: isAllPokemonLoading } = useGetPokemonQuery()
+  const { isLoading: isRandomPokemonLoading } = useGetRandomPokemonQuery()
+  console.log('Pokemons', isAllPokemonLoading)
+  console.log('Random Pokemon', isRandomPokemonLoading)
+
   return (
     <>
       <GlobalStyle />
 
-      <Layout>
-        <RandomPokemon />
-        <section>
-          <h1>Work in progress.</h1>
-        </section>
-      </Layout>
+      {isRandomPokemonLoading || isAllPokemonLoading ? (
+        <Loader />
+      ) : (
+        <Layout>
+          <RandomPokemon />
+          <section>
+            <h1>Work in progress.</h1>
+          </section>
+        </Layout>
+      )}
     </>
   )
 }
