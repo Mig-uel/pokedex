138fb825641fc2e833ab7f3769f15b14df0c2741
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-18 18:32:39 -0400

    created pokebal loader from @vinztt on codepen.

---
+++ src/components/loader/loader.component.jsx
@@ -0,0 +1,11 @@
+import { LoaderContainer, Pokeball } from './loader.styles'
+
+const Loader = () => {
+  return (
+    <LoaderContainer>
+      <Pokeball></Pokeball>
+    </LoaderContainer>
+  )
+}
+
+export default Loader
